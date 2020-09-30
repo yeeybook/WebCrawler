3707b75af2b96de35d60ae61c337b5d2a697c86d
yeeybook <58547845+yeeybook@users.noreply.github.com>
2020-09-30 19:16:10 +0900

    Update daum_webtoon_crawler.py

--- daum_webtoon_crawler.py
+++ daum_webtoon_crawler.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from time import sleep
 import pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 url = 'http://webtoon.daum.net/'
 week = ['#day=mon&tab=day', '#day=tue&tab=day', '#day=wed&tab=day', '#day=thu&tab=day', '#day=fri&tab=day', '#day=sat&tab=day', '#day=sun&tab=day']
@@ -13,7 +16,7 @@ for i in range(7): #월요일부터 일요일까지
     driver = webdriver.Chrome('C:/chromedriver.exe')
     driver.get(URL) #요일별로 링크 가져옴
 
-    sleep(10)
+    sleep(1)
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser') # 제목, 작가, 요일 긁어오기 위해 현재 페이지 파싱
@@ -39,18 +42,23 @@ for i in range(7): #월요일부터 일요일까지
         day_list.append(day.text) # 요일 리스트에 추가
         platform_list.append('다음 웹툰') # 플랫폼 리스트에 추가
 
-        sleep(10)
-
-        page = driver.find_elements_by_class_name('link_wt')[:-10] # 밑에 있는 랭킹 10개는 가져올 필요 없음
+        page = WebDriverWait(driver, 15).until( # 필요한 데이터가 다 로딩될 때까지 최대 15초 기다려줌
+            EC.presence_of_all_elements_located(
+                (By.CLASS_NAME, "link_wt")
+            )
+        )[:-10] # 밑에 있는 랭킹 10개는 가져올 필요 없음
+        
         page[p].click()
         p += 1
 
-        sleep(10)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, 'txt_genre')
+            )
+        )
 
         html =  driver.page_source
         soup = BeautifulSoup(html, 'html.parser') #장르, 줄거리 가져오기 위해 현재 페이지 파싱
-
-        sleep(3)
 
         genre = soup.find('dd', {'class': 'txt_genre'}).text.strip() # 장르 수집 (strip은 개행 제거 함수)
         genre_list.append(genre) # 장르 리스트에 추가
@@ -60,7 +68,7 @@ for i in range(7): #월요일부터 일요일까지
 
         driver.back()
 
-        sleep(5)
+        sleep(3)
 
 ############################################크롤링 끝############################################
 
